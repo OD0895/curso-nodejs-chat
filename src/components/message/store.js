ec564b0d4411fca180651c5d5ae0c34bbb0b7abd
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-08-11 14:05:21 -0500

    :construction: Gestionar conexiones a la base de datos desde la API

--- src/components/message/store.js
+++ src/components/message/store.js
@@ -1,12 +1,4 @@
-const db = require('mongoose');
 const Model = require('./model');
-
-db.Promise = global.Promise;
-db.connect('mongodb://db_user_platzivideos:odas0314@cluster0-shard-00-00.aokru.mongodb.net:27017,cluster0-shard-00-01.aokru.mongodb.net:27017,cluster0-shard-00-02.aokru.mongodb.net:27017/chatnodejs?ssl=true&replicaSet=atlas-x8y95n-shard-0&authSource=admin&retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-console.log('[db] MongoDB connected');
 
 function addMessage(message) {
   const myMessage = new Model(message);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 // Importar la funcion router
 const router = require('./network/routes');
+const db = require('./db/db');
+
+const URI = 'mongodb://db_user_platzivideos:odas0314@cluster0-shard-00-00.aokru.mongodb.net:27017,cluster0-shard-00-01.aokru.mongodb.net:27017,cluster0-shard-00-02.aokru.mongodb.net:27017/chatnodejs?ssl=true&replicaSet=atlas-x8y95n-shard-0&authSource=admin&retryWrites=true&w=majority';
+db(URI);
 
 var app = express();
 var port = 3000;
